f9405771b80309916138bb43f8504365c883e13e
Sebastian <sebastianploch@protonmail.com>
2022-03-02 12:54:07 +0000

    UE Project Init

---
+++ Source/FYP/FYP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FYP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FYP, "FYP" );
---
+++ Source/FYP/FYPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FYPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FYP_API AFYPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
