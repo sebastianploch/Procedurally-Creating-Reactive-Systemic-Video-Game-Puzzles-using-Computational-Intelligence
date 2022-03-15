4bc6d056cd73d62fab4eee03951d6d44b189bd04
Sebastian <sebastianploch@protonmail.com>
2022-03-15 16:26:06 +0000

    Added Puzzle Actor C++ Class

---
+++ Source/FYP/Game/PuzzleActor.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PuzzleActor.generated.h"
+
+UENUM(BlueprintType)
+enum class EPuzzleType : uint8
+{
+	None = 0 UMETA(DisplayName="None"),
+	Start UMETA(DisplayName="Start"),
+	Intermediate UMETA(DisplayName="Intermediate"),
+	Finish UMETA(DisplayName="Finish")
+};
+
+UCLASS()
+class FYP_API APuzzleActor final : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APuzzleActor();
+
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	EPuzzleType Type{EPuzzleType::None};
+};
