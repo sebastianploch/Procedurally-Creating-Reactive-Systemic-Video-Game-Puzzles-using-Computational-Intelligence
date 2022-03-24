6863d0e4d6c4f3fb79e5fc1d8b9756290feb5e2d
Sebastian <sebastianploch@protonmail.com>
2022-03-24 19:12:41 +0000

    Clean-up toolbar

--- Source/PuzzleSequencerEditor/Private/AssetEditor/AssetEditorToolbar_PSE.cpp
+++ Source/PuzzleSequencerEditor/Private/AssetEditor/AssetEditorToolbar_PSE.cpp
@@ -1,7 +1,6 @@
 ﻿#include "AssetEditor/AssetEditorToolbar_PSE.h"
 #include "AssetEditor/AssetEditor_PSE.h"
 #include "AssetEditor/EditorCommands_PSE.h"
-#include "AssetEditor/PSEStyle.h"
 
 #define LOCTEXT_NAMESPACE "AssetEditorToolbar_PSE"
 
@@ -15,9 +14,8 @@ void FAssetEditorToolbar_PSE::AddPuzzleSequencerToolbar(TSharedPtr<FExtender> In
 	check(Editor.IsValid());
 	const TSharedPtr<FAssetEditor_PSE> editor = Editor.Pin();
 
-	const TSharedPtr<FExtender> tbExtender = MakeShareable(new FExtender);
-	tbExtender->AddToolBarExtension("Asset", EExtensionHook::After, editor->GetToolkitCommands(), FToolBarExtensionDelegate::CreateSP(this, &FAssetEditorToolbar_PSE::FillPuzzleSequencerGraphToolbar));
-	editor->AddToolbarExtender(tbExtender);
+	InExtender->AddToolBarExtension("Asset", EExtensionHook::After, editor->GetToolkitCommands(), FToolBarExtensionDelegate::CreateSP(this, &FAssetEditorToolbar_PSE::FillPuzzleSequencerGraphToolbar));
+	editor->AddToolbarExtender(InExtender);
 }
 
 void FAssetEditorToolbar_PSE::FillPuzzleSequencerGraphToolbar(FToolBarBuilder& InToolBarBuilder)
