b5957aa348eb9171a350400fdddb8157c64dbbf2
Sebastian <sebastianploch@protonmail.com>
2022-04-03 16:51:46 +0100

    Working training for implementation2

--- Content/Python/PuzzleSequencerRL.py
+++ Content/Python/PuzzleSequencerRL.py
@@ -190,51 +190,6 @@ class GameState:
 # print(gs.get_map())
 # print("\n")
 
-
-# class GameStateOld:
-#     game_states = {"Idle": 0, "In-Progress": 1, "Finished": 2}
-#
-#     def __init__(self, puzzles, terminal_puzzle):
-#         self.current_state = self.game_states["Idle"]
-#         self.puzzles = puzzles
-#         self.terminal_puzzle = terminal_puzzle
-#
-#     def step(self, action):
-#         is_terminal = False
-#         reward = 0.1
-#
-#         # Early-out if game state is finished
-#         if self.current_state == self.game_states["Finished"]:
-#             return -1, True
-#
-#         # Change state to in-progress if stepping idle state
-#         if self.current_state is self.game_states["Idle"]:
-#             self.current_state = self.game_states["In-Progress"]
-#
-#         # Update puzzle pieces with action
-#         for puzzle in self.puzzles:
-#             if puzzle.update_puzzle(action):
-#                 reward += 0.25  # Increase reward if puzzle succeeded with provided action
-#
-#                 # Terminate if the terminal puzzle is reached and completed
-#                 if puzzle is self.terminal_puzzle:
-#                     self.current_state = self.game_states["Finished"]
-#                     is_terminal = True
-#                     reward = 1
-#                     break
-#             else:
-#                 reward -= 0.15
-#
-#         print(f"action: {action}, reward: {reward}, terminal: {is_terminal}")
-#         return reward, is_terminal
-#
-#     def get_current_state(self):
-#         return self.current_state
-#
-#     def get_puzzles(self):
-#         return self.puzzles
-
-
 # ----------------------------- NEURAL NETWORK -----------------------------
 class DQN(nn.Module):
 
@@ -248,16 +203,6 @@ class DQN(nn.Module):
         self.replay_memory_size = 50
         self.minibatch_size = 32
 
-        # self.conv1 = nn.Conv2d(3, 32, 8, 4)
-        # self.relu1 = nn.ReLU(inplace=True)
-        # self.conv2 = nn.Conv2d(32, 64, 4, 2)
-        # self.relu2 = nn.ReLU(inplace=True)
-        # self.conv3 = nn.Conv2d(64, 64, 3, 1)
-        # self.relu3 = nn.ReLU(inplace=True)
-        # self.fc4 = nn.Linear(3136, 512)
-        # self.relu4 = nn.ReLU(inplace=True)
-        # self.fc5 = nn.Linear(512, self.num_actions)
-
         self.layer1 = nn.Linear(4, 32, dtype=torch.float64)
         self.relu1 = nn.ReLU(inplace=True)
         self.layer2 = nn.Linear(128, 512, dtype=torch.float64)
@@ -265,24 +210,12 @@ class DQN(nn.Module):
         self.layer3 = nn.Linear(512, self.num_actions, dtype=torch.float64)
 
     def forward(self, x):
-        # out = self.conv1(x)
-        # out = self.relu1(out)
-        # out = self.conv2(out)
-        # out = self.relu2(out)
-        # out = self.conv3(out)
-        # out = self.relu3(out)
-        # out = out.view(out.size()[0], -1)
-        # out = self.fc4(out)
-        # out = self.relu4(out)
-        # out = self.fc5(out)
-
         out = self.layer1(x)
         out = self.relu1(out)
         out = out.view(out.size()[0], -1)
         out = self.layer2(out)
         out = self.relu2(out)
         out = self.layer3(out)
-
         return out
 
 
@@ -381,7 +314,7 @@ def train(model, start, losses, q_values):
 
         q_value = torch.sum(model(state_batch) * action_batch, dim=1)  # extract the Q-value
         # q_value = model(state_batch).gather(1, action_batch)  # extract the Q-value
-        q_values = q_value
+        q_values.append(q_value[-1].cpu().item())
 
         optimiser.zero_grad()  # reset the gradients
 
@@ -472,157 +405,22 @@ def main(mode):
         fig, ax = plt.subplots()
         iterations = np.arange(0, len(losses)).tolist()
         ax.plot(iterations, losses, linewidth=2.0)
+        ax.set_title("Loss over iterations")
+        ax.set_xlabel("iterations")
+        ax.set_ylabel("loss")
 
-        # fig2, ax2 = plt.subplots()
-        # ax2.plot(iterations, q_values, linewidth=2.0)
+        fig2, ax2 = plt.subplots()
+        ax2.plot(iterations, q_values, linewidth=2.0)
+        ax2.set_title("Q values over iterations")
+        ax2.set_xlabel("iterations")
+        ax2.set_ylabel("Q values")
 
-        plt.show(block=True)
+        if is_ipython:
+            display.clear_output(wait=True)
+            display.display(plt.gcf())
+        else:
+            plt.show(block=True)
 
 
 if __name__ == "__main__":
     main(sys.argv[1])
-
-# # # Cyclic buffer used to hold observed transitions
-# class ReplayMemory(object):
-#     def __init__(self, capacity):
-#         self.memory = deque([], maxlen=capacity)
-#
-#     def push(self, *args):
-#         """Save a Transition"""
-#         self.memory.append(Transition(*args))
-#
-#     def sample(self, batch_size):
-#         return random.sample(self.memory, batch_size)
-#
-#     def __len__(self):
-#         return len(self.memory)
-
-# # Q Network class
-# class DQN(nn.Module):
-#     def __init__(self, h, w, outputs):
-#         super(DQN, self).__init__()
-#         self.conv1 = nn.Conv2d(4, 16, kernel_size=5, stride=2)
-#         self.bn1 = nn.BatchNorm2d(16)
-#         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-#         self.bn2 = nn.BatchNorm2d(32)
-#         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-#         self.bn3 = nn.BatchNorm2d(32)
-#
-#         # Number of Linear input connections depends on output of conv2d layers
-#         def conv2d_size_out(size, kernel_size=5, stride=2):
-#             return (size - (kernel_size - 1) - 1) // stride + 1
-#
-#         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-#         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-#         linear_input_size = convw * convh * 32
-#         self.head = nn.Linear(linear_input_size, outputs)
-#
-#     def forward(self, x):
-#         x = x.to(Device)
-#         x = F.relu(self.bn1(self.conv1(x)))
-#         x = F.relu(self.bn2(self.conv2(x)))
-#         x = F.relu(self.bn3(self.conv3(x)))
-#         return self.head(x.view(x.size(), -1))
-#
-#
-# # Training - Hyperparameters
-# BATCH_SIZE = 128
-# GAMMA = 0.999
-# EPSILON_START = 0.9
-# EPSILON_END = 0.05
-# EPSILON_DECAY = 200
-# TARGET_UPDATE = 10
-
-# Training
-
-# # Get number of actions from gym action space
-# n_actions = env.action_space.n
-#
-# policy_net = DQN(screen_height, screen_width, n_actions).to(device)
-# target_net = DQN(screen_height, screen_width, n_actions).to(device)
-# target_net.load_state_dict(policy_net.state_dict())
-# target_net.eval()
-#
-# optimizer = optim.RMSprop(policy_net.parameters())
-#
-# steps_done = 0
-# memory = ReplayMemory(10000)
-
-# def select_action(state):
-#     global steps_done
-#     sample = random.random()
-#     epsilon_threshold = EPSILON_END + (EPSILON_START - EPSILON_END) * \
-#                         math.exp(-1. * steps_done / EPSILON_DECAY)
-#     steps_done += 1
-#     if sample > epsilon_threshold:
-#         with torch.no_grad():
-#             return  # policy_net(state).max(1)[1].view(1, 1)
-#     else:
-#         return  # torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
-#
-#
-# episode_durations = []
-#
-#
-# def plot_durations():
-#     plt.figure(2)
-#     plt.clf()
-#     durations_t = torch.tensor(episode_durations, dtype=torch.float)
-#     plt.title("Training...")
-#     plt.xlabel("Episode")
-#     plt.ylabel("Duration")
-#     plt.plot(durations_t.numpy())
-#
-#     # Take 100 episode averages and plot them
-#     if len(durations_t) >= 100:
-#         means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-#         means = torch.cat((torch.zeros(99), means))
-#         plt.plot(means.numpy())
-#
-#     plt.pause(0.001)
-#     if is_ipython:
-#         display.clear_output(wait=True)
-#         display.display(plt.gcf())
-#
-#
-# # Training Loop
-# def optimise_model():
-#     if len(memory) < BATCH_SIZE:
-#         return
-#     transitions = memory.sample(BATCH_SIZE)
-#     batch = Transition(*zip(*transitions))
-#     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)),
-#                                   device=Device, dtype=torch.bool)
-#     non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
-#
-#     state_batch = torch.cat(batch.state)
-#     action_batch = torch.cat(batch.action)
-#     reward_batch = torch.cat(batch.reward)
-#
-#     # # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-#     # # columns of actions taken. These are the actions which would've been taken
-#     # # for each batch state according to policy_net
-#     # state_action_values = policy_net(state_batch).gather(1, action_batch)
-#     #
-#     # # Compute V(s_{t+1}) for all next states.
-#     # # Expected values of actions for non_final_next_states are computed based
-#     # # on the "older" target_net; selecting their best reward with max(1)[0].
-#     # # This is merged based on the mask, such that we'll have either the expected
-#     # # state value or 0 in case the state was final.
-#     # next_state_values = torch.zeros(BATCH_SIZE, device=device)
-#     # next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
-#     # # Compute the expected Q values
-#     # expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-#
-#     # # Compute Huber Loss
-#     # critetion = nn.SmoothL1Loss()
-#     # loss = critetion(state_actions_values, expected_state_action_values.unsqueeze(1))
-#
-#     # # Optimize the model
-#     # optimizer.zero_grad()
-#     # loss.backward()
-#     # for param in policy_net.parameters():
-#     #     param.grad.data.clamp_(-1, 1)
-#     # optimizer.step()
-#
-# # Main Loop
